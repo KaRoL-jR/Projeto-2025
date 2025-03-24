ec70b57f79891bff775ab17d289991d4d75be66e
KaRoL-jR <ana_karoline2004@outlook.com>
2025-03-23 21:55:59 -0300

    Create ex006.py
    conversor de cripto moedas

---
+++ ex006.py
@@ -0,0 +1,9 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantas cripto ela poderá comprar.
+
+real = float(input('Quantos reais você possui na carteira R$: '))
+rxp = real * 12.13
+rs = float(205 * 5.81) / 12.13
+
+print('=' * 12)
+print(f'no total de RXP você tem {:.2f}')
+print('=' * 12)
