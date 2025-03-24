08f4362d6cf1e2b60d2ffcc6cd797a28624ff0fb
KaRoL-jR <ana_karoline2004@outlook.com>
2025-03-23 21:31:27 -0300

    Create ex002.py
    algoritimo em python que mostra o dobro, raiz quadrada e o triplo de um número

---
+++ ex002.py
@@ -0,0 +1,12 @@
+#Crie um algoritimo que leia um número e mostre seu dobro, raiz quadrada e triplo. 
+
+num = float(input('Digite um valor: '))
+dobro = num * 2
+raiz = num ** 0.5
+triplo = num * 3
+
+print('-' * 12)
+print(f'O dobro de {num} é {dobro})
+print(f'A raiz quadrada de {num} é {raiz:.2f})
+print('-' * 12)
+                  
