8cf1aa5656d9c7401094ca7c6c5101ed129849ba
KaRoL-jR <ana_karoline2004@outlook.com>
2025-03-23 21:40:25 -0300

    Create ex03.py
    programa que lê a média do aluno

---
+++ ex03.py
@@ -0,0 +1,11 @@
+#Desenvolva um programa que leia a média de um aluno
+
+n1 = float(input('Mostre sua nota da primeira unidade:'))
+n2 = float(input('Mostre sua nota da segunda unidade: '))
+n3 = float(input('Mostre sua nota da terceira unidade:'))
+n4 = float(input('Mostre sua nota da quarta unidade: '))
+média = (n1+n2+n3+n4) / 4
+
+print('-' * 12)
+print(f'A sua média das suas notas {:.1f}')
+print('-' * 12)
